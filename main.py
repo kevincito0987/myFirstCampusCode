841d63b6c156ea2198f56427dd51abb26b86cec1
kevincito0987 <kevinangaritacaamano@gmail.com>
2024-12-03 07:50:35 -0500

    Modificaciones

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+#Tipos de datos
+name = "Kevin Angarita"
+age = 21
+camper = True
+
+#Listas
+hobiies = ()
+hobiies = ["Cocinar", "Estudiar", "Programar", "Jugar Videojuegos"]
+dato = input("Ingrese un pasastiempo: ")
+hobiies.append(dato)
+print(len(hobiies))
+#pop(eliminar el ultimo)
