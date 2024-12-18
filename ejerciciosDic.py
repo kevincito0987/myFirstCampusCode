8eee2f120b51aa0d3320eed69f07c7cec4d86018
kevincito0987 <kevinangaritacaamano@gmail.com>
2024-12-18 17:14:28 -0500

    El primero el

---
+++ ejerciciosDic.py
@@ -0,0 +1,45 @@
+#Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'\'}, pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+print("********")
+print("Programa para saber si la divisa que ingreses esta dentro de mi programacion.")
+def solicitar_divisa():
+   while True:
+        cadena = input("Ingrese el nombre de una divida, el nombre que contenga la primera letra mayuscula y las demas en minusculas: ")
+        if cadena.isalpha():
+            return cadena
+        else:
+            print("Entrada inválida. Por favor, ingrese solo letras.")
+
+def divisa_existe(nombre_divisa, lista_divisas):
+  """Verifica si un código de divisa existe y devuelve la información si es así."""
+
+  for divisa in lista_divisas:
+    if divisa["nombre"] == nombre_divisa:
+      return divisa
+  return None
+
+lista_divisas = [
+    {"nombre": "Dólar estadounidense", "código": "USD", "símbolo": "$"},
+    {"nombre": "Euro", "código": "EUR", "símbolo": "€"},
+    {"nombre": "Libra esterlina", "código": "JPY", "símbolo": "¥"},
+    {"nombre": "Yen japonés", "código": "JPY", "símbolo": "¥"},
+    {"nombre": "Dólar australiano", "código": "AUD", "símbolo": "A$"},
+    {"nombre": "Dólar canadiense", "código": "CAD", "símbolo": "C$"},
+    {"nombre": "Franco suizo", "código": "CHF", "símbolo": "CHF"},
+    {"nombre": "Renminbi/yuan chino	", "código": "CNY", "símbolo": "元"},
+    {"nombre": "Dólar de Hong Kong", "código": "HKD	", "símbolo": "HK$"},
+    {"nombre": "Dólar de Nueva Zelanda", "código": "NZD	", "símbolo": "NZ$"},
+]
+
+while True:
+  nombre_divisa = solicitar_divisa()
+  if nombre_divisa.lower() == 'salir':
+    break
+
+  divisa_encontrada = divisa_existe(nombre_divisa, lista_divisas)
+
+  if divisa_encontrada:
+    print(f"Información de la divisa {nombre_divisa}:")
+    print(f"Nombre: {divisa_encontrada['nombre']}")
+    print(f"Símbolo: {divisa_encontrada['símbolo']}")
+  else:
+    print(f"La divisa {nombre_divisa} no existe en nuestra base de datos.")
